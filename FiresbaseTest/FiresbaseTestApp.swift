ec1c39003a0a9d1abaae213d1c4f08cb5f4dba37
howie1329 <46204910+howie1329@users.noreply.github.com>
2023-02-24 21:02:24 -0500

    Initial Commit

---
+++ FiresbaseTest/FiresbaseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiresbaseTestApp.swift
+//  FiresbaseTest
+//
+//  Created by Howard Thomas on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct FiresbaseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
